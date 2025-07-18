63031c388a2d60053b6cdbb8bd7f81439a0a5ac3
sharpslice <davidlein1440p@gmail.com>
2025-07-18 12:38:20 -0700

    style: seperate header from main container where content will lie

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header/Header"
-
+import './App.css'
 
 function App() {
   return (
     <>
       <Header/>
+
+      <div className="main-container">
+          <Outlet/>
+      </div>
       
-      <Outlet/>
     </>
   )
 }
--- frontend/src/HomePage/HomePage.jsx
+++ frontend/src/HomePage/HomePage.jsx
@@ -1,6 +1,9 @@
+import './HomePage'
 function HomePage(){
     return(
-        <h1>{"This is home page"}</h1>
+        <div className="homepage">
+            message board
+        </div>
     )
 }
 export default HomePage
