5bebb4d24a6d1b5b9fed4e937c7424fb2c5246ad
sharpslice <davidlein1440p@gmail.com>
2025-07-18 18:13:01 -0700

    feat: add log-in form - remember me option for session - forgot password - sign up

--- frontend/src/LandingPage/LandingPage.jsx
+++ frontend/src/LandingPage/LandingPage.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom"
-
+import './LandingPage.css'
 function LandingPage(){
     return(
         <div className="landing-container">
--- frontend/src/LandingPage/LoginPage/LoginPage.jsx
+++ frontend/src/LandingPage/LoginPage/LoginPage.jsx
@@ -1,6 +1,44 @@
+import { Link } from 'react-router-dom'
+import './LoginPage.css'
 function LoginPage(){
     return(
-        <h1>Login page</h1>
+        <div className="log-in__container">
+
+            <div className='log-in__banner'>
+                LOG IN
+            </div>
+
+            <form className="log-in__form" action="" method="post">
+                <div className='log-in__form-group'>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id='username' name ='username'/> 
+                </div>
+                
+                <div className='log-in__form-group'>
+                    <label htmlFor='password'>Password</label>
+                    <input type="password" id='password' name='password'/>
+                </div>
+
+                <div className='form-options'>
+
+                    <div className='form-options__remember-me'>
+                        
+                        <input type='checkbox' id ='remember-me' name='remember-me'/>
+                        <label htmlFor='remember-me'>Remember me</label>
+                        
+                    </div>
+                    <Link className='form-options__link' to="">Forgot Password</Link>
+                </div>
+                
+                <button className='log-in__button' type='submit'>Sign in</button>
+            </form>
+
+            <div className='log-in__signup'>
+                <span>Don't have an account? </span>
+                <Link >Sign up</Link>
+            </div>
+            
+        </div>
     )
 }
 
