29d10d06b88d59ea0c54eef75058c21ba30134d6
sharpslice <davidlein1440p@gmail.com>
2025-07-18 18:57:24 -0700

    refactor: change form submit to work with react

--- frontend/src/LandingPage/LoginPage/LoginPage.jsx
+++ frontend/src/LandingPage/LoginPage/LoginPage.jsx
@@ -1,6 +1,17 @@
 import { Link } from 'react-router-dom'
 import './LoginPage.css'
+import { useState } from 'react';
 function LoginPage(){
+    const [username,setUsername] = useState('')
+    const [password,setPassword] = useState('')
+
+
+    const handleSubmit=(e)=>
+    {
+        e.preventDefault();
+        alert(`username: ${username} password: ${password}`)
+    }
+
     return(
         <div className="log-in__container">
 
@@ -11,12 +22,23 @@ function LoginPage(){
             <form className="log-in__form" action="" method="post">
                 <div className='log-in__form-group'>
                     <label htmlFor="username">Username</label>
-                    <input type="text" id='username' name ='username'/> 
+                    <input 
+                        type="text" 
+                        id='username' 
+                        value={username}
+                        onChange={(e)=>setUsername(e.target.value)}
+                    /> 
                 </div>
                 
                 <div className='log-in__form-group'>
                     <label htmlFor='password'>Password</label>
-                    <input type="password" id='password' name='password'/>
+                    <input 
+                        type="password" 
+                        id='password' 
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
+                    
+                    />
                 </div>
 
                 <div className='form-options'>
@@ -30,7 +52,7 @@ function LoginPage(){
                     <Link className='form-options__link' to="">Forgot Password</Link>
                 </div>
                 
-                <button className='log-in__button' type='submit'>Sign in</button>
+                <button className='log-in__button' onClick={(e)=>handleSubmit(e)}>Sign in</button>
             </form>
 
             <div className='log-in__signup'>
