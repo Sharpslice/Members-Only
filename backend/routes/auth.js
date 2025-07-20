dc0bb7dabee7199f86350a4a3cd52b3be02d2bc7
sharpslice <davidlein1440p@gmail.com>
2025-07-19 21:53:51 -0700

    add password authentification

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,9 @@
 const auth = require('express').Router()
 const bcrypt = require('bcrypt')
-const pool = require('../db.js')
-auth.post('/sign-up',async(req,res)=>{
+const pool = require('../db.js');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+auth.post('/sign-up',async(req,res,next)=>{
     const {username,password,name,lastName} = req.body;
 
     const hashedPassword = await bcrypt.hash(password,10);
@@ -13,11 +15,55 @@ auth.post('/sign-up',async(req,res)=>{
         `,[username,name,lastName,hashedPassword])
 
         res.status(200).send('good')
-        
+
     }
     catch(error){
         next(error)
     }
 })
+
+passport.use(new LocalStrategy(async function verify (username,password,done){
+    try{
+        
+        const {rows} = await pool.query(`SELECT * FROM users WHERE username = $1`,[username]);
+        const user = rows[0];
+        if(!user) return done(null,false,{message:'incorrect username'});
+        const hashedPassword = user.password;
+        if( !bcrypt.compare(password,hashedPassword)) return done(null,false, {message: 'incorrect password'});
+
+        console.log('credentials authenticateds')
+        return done(null,user);
+
+    }catch(err){
+       
+        return done(err);
+    }
+}))
+
+passport.serializeUser(function(user,done){
+    return done(null,user.id)
+})
+
+passport.deserializeUser(async(id,done)=>{
+    try{
+        const {rows} = await pool.query(`SELECT * from users WHERE id=$1 `,id);
+        const user = rows[0];
+        done(null,user)
+    }catch(error){
+        done(error)
+    }
+})
+
+auth.post('/log-in',passport.authenticate('local'),
+    (req,res)=>{
+       
+        res.status(200).json({message:'successful log-in'})
+    }
+
+
+)
+
+
+
 
 module.exports = auth;
