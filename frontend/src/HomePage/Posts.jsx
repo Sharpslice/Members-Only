f74aca7b7bd506bc47df9465065c94efba0cd8a3
sharpslice <davidlein1440p@gmail.com>
2025-07-22 01:26:45 -0700

    wip: add draggabl modal for opening user's account through post's author's post -modal gets reset upon redragging again -modal is hard to drag due to selectable text

--- frontend/src/HomePage/Posts.jsx
+++ frontend/src/HomePage/Posts.jsx
@@ -1,26 +1,96 @@
 import axios from 'axios';
 import './Posts.css'
+import { useRef } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function Posts({title,body,created_at,user_id}){
+    const dialogRef = useRef();
 
+    
+
+   
     const onHandleClick=async()=>{
         const response = await axios.get(`http://localhost:3000/user/${user_id}`)
         console.log(response.data.user)
+        if(!dialogRef.current.open){
+            dialogRef.current.show();
+        }
+        
     }
 
-    return(
-        <div className="posts" onClick={onHandleClick}>
-            <div>
-                <h1>{title}</h1>
-            </div>
-            <div>
-                {body}
-            </div>
-            <div>
-                {created_at}
-            </div>
+    
+    const [mouseClick,setMouseClick] = useState(false);
 
-        </div>
+    const [modalX,setModalX] = useState()
+    const [modalY,setModalY] = useState()
+    const[x,setX]=useState(null)
+    const[y,setY]= useState(null)
+
+  
+
+   
+
+    const onMouseDown=(e)=>{
+        
+        setMouseClick(true)
+      
+        const rect = dialogRef.current.getBoundingClientRect()
+        
+        setModalX(rect.left)
+        setModalY(rect.top)
+      
+    }
+    const onMouseUp=(e)=>{
+       
+        setMouseClick(false)
+        console.log('lifting up')
+        
+    }
+    const onMouseMove=(e)=>{
+        if(!mouseClick) return;
+        
+        const currentX = (e.clientX - modalX)
+        const currentY = (e.clientY - modalY)
+        const thisx = currentX
+        const thisy = currentY
+        
+        setX(thisx);
+        setY(thisy);
+        
+        
+ 
+        
+        
+    }
+
+    
+
+    return(
+        <>
+            <div className="posts" onClick={onHandleClick}>
+                <div>
+                    <h1>{title}</h1>
+                </div>
+                <div>
+                    {body}
+                </div>
+                <div>
+                    {created_at}
+                </div>
+
+            </div>
+            <dialog 
+                ref={dialogRef}
+                onMouseDown={onMouseDown}
+                onMouseUp={onMouseUp}
+                onMouseMove={onMouseMove}
+                onMouseLeave={()=>{setMouseClick(false)}}
+                style={{width:'5rem',height:'2rem',transform: `translate(${x}px,${y}px)`}}
+            >
+                <p>{user_id}</p>
+            </dialog>
+        </>
     )
 }
 
